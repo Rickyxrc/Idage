f120526f0f8ca3fff5687b3312214c53de1e5f25
Rickyxrc <rickyxrc@outlook.com>
2022-08-28 18:53:01 +0800

    add feat:侦听端口自定义

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const cors=require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-console.log('checking environment......');
+console.log("checking environment......");
 if (process.env.GITHUBTOKEN == undefined) {
-    console.log('please set environment variable "GITHUBTOKEN".')
-    process.exit();
+  console.log('please set environment variable "GITHUBTOKEN".');
+  process.exit();
 }
 
 app.use(cors());
 
-console.log('loading router......');
-app.get('/github/user',require('./api/github/user'));
-app.get('/github/repo', require('./api/github/repo'));
-app.get('/luogu/user',require('./api/luogu/user'));
-console.log('router loaded.')
+console.log("loading router......");
+app.get("/github/user", require("./api/github/user"));
+app.get("/github/repo", require("./api/github/repo"));
+app.get("/luogu/user", require("./api/luogu/user"));
+console.log("router loaded.");
 
-
-console.log('server listening on port 80.');
-app.listen(80);
+console.log("server listening on port 80.");
+app.listen(process.env.PORT | 80);
